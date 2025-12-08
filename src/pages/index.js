a2fc13c2db686aab482ca04717d6d022cb002f07
pravn27 <praveen.sri27@gmail.com>
2025-12-08 22:14:19 +0530

    Typescript tech doc - update

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,5 @@ import { Redirect } from "@docusaurus/router";
 
 // make default landing page as docs
 export default function Home() {
-  return <Redirect to="/javascript-tech-doc/docs/category/documentations" />;
+  return <Redirect to="/typescript-tech-doc/docs/category/documentations" />;
 }
